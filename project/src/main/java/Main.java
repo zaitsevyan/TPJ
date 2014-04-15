1add2319e263349380fe6066a09c71e2db9a062c
Yan Zaitsev <yan.zaitsev@zentity.com>
2014-04-16 00:17:41 +0200

    Swing support

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: zaitsevyan
@@ -6,6 +8,6 @@
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello in console!");
+        JOptionPane.showMessageDialog(null,"Hello from SWING! (c) Yan");
     }
 }
