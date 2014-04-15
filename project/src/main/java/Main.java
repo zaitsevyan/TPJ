7f94ede48eec6ff206ce728012f53dfb3eb4110c
Yan Zaitsev <yan.zaitsev@zentity.com>
2014-04-15 18:52:59 +0200

    Project import

---
+++ project/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zaitsevyan
+ * Date: 15/04/14
+ * Time: 18:48
+ */
+public class Main {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"Hello");
+    }
+}
